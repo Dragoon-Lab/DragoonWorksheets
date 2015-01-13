83ab0c654dc667b310a7658b260379978a062260
MackenzieKong-Sievrt <mkong.sivert@gmail.com>
2015-01-13 14:57:25 -0700

    Done except for the loose text and loose images

--- worksheet_generator2.py
+++ worksheet_generator2.py
@@ -26,15 +26,21 @@ class Table:
 		self.rows = []
 		line = input("Line: ").split(",")
 		while not "end" in line:
+			line2 = []
 			for item in line:
 				if item == "dropdown":
-					ans = input ("Answers: ").split(",")
-					rta = input ("Right answer: ")
-					if not rta in ans:
+					item = []
+					options = input ("Answers: ").split(",")
+					correct = input ("Right answer: ")
+					if not correct in options:
 						print("Please choose one of the given answers as the right answer.")
-						rta = input ("Right answer: ")
-					print (ans) 	
-			self.rows.append(line)			
+						correct = input ("Right answer: ")
+					item.append(options)
+					item.append(correct)
+					print(item)
+					print(line2)
+				line2.append(item)	
+			self.rows.append(line2)			
 			line = input ("Line: ").split(",")			
 		sect.content.append(self)				
 							
@@ -51,6 +57,10 @@ class Dropdown:
 		sect.content.append(self)	
 	def jsonable(self):
 		return self.__dict__
+		
+class Text:
+
+class Image:
 	
 def getJsonable(obj):
     if hasattr(obj, 'jsonable'):
@@ -66,8 +76,12 @@ def getJsonable(obj):
 def generateHTMLWorksheet(wks):
 	# Converts Worksheet object, "wks" into an html/javascript file
 	worksheet = "<!DOCTYPE html>\n<html>\n<head>\n<title>Dragoon Worksheet 1</title>\n<script type=\"text/javascript\">\nfunction checkAnswers(inputId, rightAnswer) {\nif (document.getElementById(inputId).value===rightAnswer) {\ndocument.getElementById(inputId).style.background=\"#66FF33\";\nreturn true;\n}\nelse {\ndocument.getElementById(inputId).style.background=\"#FF3333\";\nreturn false;\n}\n};\nvar tim1 = 0;\nfunction time1 () {\nif (yestim1=1){\ntim1 = tim1 + 1;\nt = setTimeout(function() {time1()},1000);\n}\n};\nvar yestim1 = 1;\ntime1();\n</script>\n</head>\n<body>"
+	divset = ""
+	endtbl = "<div id=\"resultsTable\" style=\"display:none\">\n<table>\n<thead>\n<td style=\"border: 2pt black solid\">Question</td>\n<td style=\"border: 2pt black solid\">Correct Answer</td>\n<td style=\"border: 2pt black solid\">Student Answers</td>\n<td style=\"border: 2pt black solid\">Number of Tries</td>\n<td style=\"border: 2pt black solid\">Time for Entire Question (s)</td>\n</thead>\n<tbody>"
+	endfn = "<script type=\"text/javascript\">\nfunction displayAnswers () {\ndocument.getElementById(\"resultsTable\").style.display=\"\""
 	alph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 	rnum = ["i","ii","iii","iv","v","vi","vii","viii","ix","x","xi","xii","xiii","xiv","xv","xvi","xvii","xviii","xix","xx","xxi","xxii","xxiii","xxiv","xxv"]
+	docname = wks.name + ".html"
 	for item in wks.questions:#These items are questions.
 		ques = item
 		qno = str(wks.index(ques) + 1)
@@ -105,9 +119,68 @@ def generateHTMLWorksheet(wks):
 						endtbl = endtbl + "</td>\n</tr>"
 					endfn = endfn + "\ndocument.getElementById(\"" + qno + let + rno + "Tries\").innerHTML=set" + qno + "." + let + rno + ";"
 				elif part.__class__.__name__ == "Table":
-					pass
-
-		
+					global allhead
+					allhead = ""
+					global allbody
+					allbody = ""
+					alline = ""
+					for item in part:
+						if part.index(item) == 0:
+							item = head
+							for item in head:
+								item = "\n<td style=\"border: 2pt black solid\">" + str(item) + "</td>"
+								allhead = allhead + item
+						else:
+							item = body
+							for item in body:
+								if isinstance(item, list):
+									item = drdn
+									allans = ""
+									for item in drdn:
+										if drdn.index(item) == 1:
+											global rta
+											rta = item
+										else:
+											item = "\n<option>" + str(item) + "</option>"
+											allans = allans + item
+										varset = varset + "\n" + lno + rno + ":0,"
+										chkans = chkans + "checkAnswers(\"" + qno + lno + rno + "\", \"" + rta + "\");"
+										chkcpl = chkcpl + "document.getElementById(\"" + qno + lno + rno + "\").value===\"\" ||"
+										ctr = ctr + "if (!checkAnswers(\"" + qno + lno + rno + "\", \"" + rta + "\")) { \n set" + qno + "." + lno + rno + " = set" + qno + "." + lno + rno + " + 1;}"
+										contif = contif + "(checkAnswers(\"" + qno + lno + rno + "\", \"" + rta + "\") || set" + qno + "." + lno + rno + "===3) &&"
+										ctwrng = ctwrng + "\nif (!checkAnswers(\"" + qno + lno + rno + "\", \"" + rta + "\")) {\nif (set" + qno + "." + lno + rno + "===3) {\ndocument.getElementById(\"" + qno + lno + rno + "\").value=\"" + rta + "\";\ndocument.getElementById(\"" + qno + lno + rno + "\").style.background=\"#FFFF00\"}\nif (set" + qno + "." + lno + rno + "=== 1) { \n document.getElementById(\"" + qno + lno + rno + "Answer1\").innerHTML = \"; \" + document.getElementById(\"" + qno + lno + rno + "\").value \n } \n else if (set" + qno + "." + lno + rno + "=== 2) { \n document.getElementById(\"" + qno + lno + rno + "Answer2\").innerHTML = \"; \" + document.getElementById(\"" + qno + lno + rno + "\").value \n } \n else { \n document.getElementById(\"" + qno + lno + rno + "Answer3\").innerHTML = \"; \" + document.getElementById(\"" + qno + lno + rno + "\").value \n } \n } \n else { \n if (set" + qno + "." + lno + rno + "=== 0) { \n document.getElementById(\"" + qno + lno + rno + "Answer1\").innerHTML = \"; \" + document.getElementById(\"" + qno + lno + rno + "\").value \n } \n else if (set" + qno + "." + lno + rno + "=== 1) { \n document.getElementById(\"" + qno + lno + rno + "Answer2\").innerHTML = \"; \" + document.getElementById(\"" + qno + lno + rno + "\").value \n } \n else { \n document.getElementById(\"" + qno + lno + rno + "Answer3\").innerHTML = \"; \" + document.getElementById(\"" + qno + lno + rno + "\").value\n}\n};"
+										allline = allline + "<td style=\"border: 2pt black solid\"><select id=\"" + qno + lno + rno + "\">\n<option></option>" + allans + "</select></td>"
+										endtbl = endtbl + "\n<tr>\n<td style=\"border: 2pt black solid\">" + qno + lno + ". " + rno + ".</td>\n<td style=\"border: 2pt black solid\">" + rta + "</td>\n<td style=\"border: 2pt black solid\"><div id=\"" + qno + lno + rno + "Answer1\"></div><div id=\"" + qno + lno + rno + "Answer2\"></div><div id=\"" + qno + lno + rno + "Answer3\"></div></td>\n<td style=\"border: 2pt black solid\"><div id=\"" + qno + lno + rno + "Tries\"></div></td>\n<td style=\"border: 2pt black solid\">"
+										if lno == "a" and rno == "i":
+											endtbl = endtbl + "<div id=\"" + qno + "Time\"></div></td>\n<tr>"
+										else:
+											endtbl = endtbl + "</td>\n</tr>"
+										endfn = endfn + "\ndocument.getElementById(\"" + qno + lno + rno + "Tries\").innerHTML=set" + qno + "." + lno + rno + ";"
+								else:
+									item = "\n<td style=\"border: 2pt black solid\">" + str(item) + "</td>"
+									allline = allline + item
+							allline = allline + "</tr>"
+							allbody = allbody + allline
+							line = input("Line: ").split(",")
+						worksheet = worksheet + "<table>\n<thead>" + allhead + "\n</thead>\n<tbody>" + allbody + "\n</tbody>\n</table>"
+		if qno < len(wks.questions):
+			varset = varset + "}; \n"
+			chkcpl = chkcpl + "0===1) {\n alert(\"It appears you have left at least one of these fields blank. Please remedy this immediately.\");\n}"
+			contif = contif + "1===1) {\n document.getElementById(\"question" + str(int(qno)+1) + "\").style.display = \"\";\n document.getElementById(\"button" + qno + "\").style.display = \"none\";\ntim" + str(int(qno)+1) + "= 0;\nfunction time" + str(int(qno)+1) + "() {\nif (yestim" + str(int(qno)+1) + "){\ntim" + str(int(qno)+1) + " = tim" + str(int(qno)+1) + " + 1;\nt = setTimeout(function() {time" + str(int(qno)+1) + "()},1000);\n}\n};\nvar yestim" + str(int(qno)+1) + " = 1;\ntime" + str(int(qno)+1) + "();\n}"
+			contfn = contfn + "\n" + chkans + "\n" + chkcpl + "\n" + ctr + "\n" + contif + "\n" + ctwrng
+			worksheet = worksheet + "\n<script type=\"text/javascript\">" + varset + contfn + "};\n</script> \n <br><button id=\"button" + qno + "\" onClick=\"cont" + qno + "();\">Continue</button>\n<div id=\"question" + str(int(qno)+1) + "\" style=\"display: none\">"
+			divset = divset + "</di
+		else:
+			varset = varset + "}; \n"
+			chkcpl = chkcpl + "0===1) {\n alert(\"It appears you have left at least one of these fields blank. Please remedy this immediately.\");\n}"
+			contif = contif + "1===1) {\n document.getElementById(\"question" + str(int(qno)+1) + "\").style.display = \"\";\n document.getElementById(\"button" + qno + "\").style.display = \"none\";\ndisplayAnswers();\n}"
+			contfn = contfn + "\n" + chkans + "\n" + chkcpl + "\n" + ctr + "\n" + contif + "\n" + ctwrng
+			worksheet = worksheet + "\n <script type=\"text/javascript\">" + varset + contfn + "};\n</script> \n <br><button id=\"button" + qno + "\" onClick=\"cont" + qno + "();\">Continue</button>\n<div id=\"question" + str(int(qno)+1) + "\" style=\"display: none\">"
+			divset = divset + "</div>"
+	endtbl = endtbl + "\n</tbody>\n</table>\n</div>"
+	endfn = endfn + "\n};\n</script>"
+	worksheet = worksheet + divset + endtbl + endfn + "</body></html>"
+	open(docname,"w").write(worksheet)		
 ######################
 ## Construct an example:
 
